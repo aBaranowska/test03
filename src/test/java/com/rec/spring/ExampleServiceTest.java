ab1c1fedd30c1d39257be3c5ce77135e6db933f2
Agnieszka Baranowska <agnieszka.baranowska@rec-offshore.intranet>
2016-03-22 15:12:21 +0100

    test03

---
+++ src/test/java/com/rec/spring/ExampleServiceTest.java
@@ -0,0 +1,46 @@
+package com.rec.spring;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ExampleServiceTest {
+
+	private static final String HELLO_WORLD = "Hello world!";
+
+	private ApplicationContext context;
+
+	@Before
+	public void setUp() {
+		context = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
+	}
+
+	@After
+	public void tearDown() {
+		((AbstractApplicationContext) context).close();
+	}
+
+	@Test
+	public void getBean01() {
+		Service bean = (Service) context.getBean("helloId");
+		assertEquals(HELLO_WORLD, bean.getMessage());
+	}
+
+	@Test
+	public void getBean02() {
+		Service bean = context.getBean("helloId", Service.class);
+		assertEquals(HELLO_WORLD, bean.getMessage());
+	}
+
+	@Test
+	public void getBean03() {
+		Service bean = context.getBean("helloName", Service.class);
+		assertEquals(HELLO_WORLD, bean.getMessage());
+	}
+
+}
